0ad44a3c7248c044dd7412bf5b5f0316fab02b06
Michael McKnight <micmcknight@outlook.com>
2021-02-25 23:32:53 +1100

    Reproduce issue with creating http api integrations

--- bin/cdk-ag2-test.ts
+++ bin/cdk-ag2-test.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkAg2TestStack } from '../lib/cdk-ag2-test-stack';
+import { CdkAg2TestStack, LambdaStack } from '../lib/cdk-ag2-test-stack';
 
 const app = new cdk.App();
-new CdkAg2TestStack(app, 'CdkAg2TestStack');
+const lambdaStack1 = new LambdaStack(app, 'LambdaStack1');
+const lambdaStack2 = new LambdaStack(app, 'LambdaStack2');
+new CdkAg2TestStack(app, 'CdkAg2TestStack', {}, lambdaStack1, lambdaStack2);
--- lib/cdk-ag2-test-stack.ts
+++ lib/cdk-ag2-test-stack.ts
@@ -1,9 +1,41 @@
 import * as cdk from '@aws-cdk/core';
 
+import * as apigateway from '@aws-cdk/aws-apigatewayv2';
+import * as apigatewayIntegrations from '@aws-cdk/aws-apigatewayv2-integrations';
+import * as lambda from '@aws-cdk/aws-lambda';
+
 export class CdkAg2TestStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: cdk.StackProps, lambdaStack1: LambdaStack, lambdaStack2: LambdaStack) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new apigateway.HttpApi(this, 'api');
+
+    api.addRoutes({
+      path: '/lambda1',
+      integration: new apigatewayIntegrations.LambdaProxyIntegration({
+        handler: lambdaStack1.lambda,
+        payloadFormatVersion: apigateway.PayloadFormatVersion.VERSION_2_0
+      })
+    });
+
+    api.addRoutes({
+      path: '/lambda2',
+      integration: new apigatewayIntegrations.LambdaProxyIntegration({
+        handler: lambdaStack2.lambda,
+        payloadFormatVersion: apigateway.PayloadFormatVersion.VERSION_2_0
+      })
+    });
+  }
+}
+
+export class LambdaStack extends cdk.Stack {
+  lambda: lambda.Function;
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    this.lambda = new lambda.Function(this, 'lambda', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`exports.handler = async function(event, context) { return { statusCode: 200, body: "Stack: ${id}" }; };`),
+    });
   }
 }
